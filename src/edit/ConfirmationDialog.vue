95a42260f521257a93d781219978be80916b95dc
ikovac <ikovacevic96k@gmail.com>
2021-02-11 17:51:36 +0100

    Implement TailorDialog inside ConfirmationDialog

--- src/edit/ConfirmationDialog.vue
+++ src/edit/ConfirmationDialog.vue
@@ -1,28 +1,22 @@
 <template>
-  <v-dialog v-model="dialog" width="500">
+  <tailor-dialog v-model="dialog" header-icon="mdi-alert" width="500">
     <template #activator="scope">
       <slot v-bind="scope"></slot>
     </template>
-    <v-card>
-      <v-card-title primary-title class="dialog-title primary darken-1">
-        <v-avatar color="secondary" size="38" class="mr-3">
-          <v-icon dark>mdi-alert</v-icon>
-        </v-avatar>
-        <div class="text-truncate white--text">Delete caption?</div>
-      </v-card-title>
-      <v-card-text class="text-left pt-7 px-4 pb-2">
-        Are you sure you want to delete caption?
-      </v-card-text>
-      <v-card-actions class="px-4 pb-3">
-        <v-spacer />
-        <v-btn @click="dialog = false" color="primary" text>Close</v-btn>
-        <v-btn @click="confirm" color="error" text>Delete caption</v-btn>
-      </v-card-actions>
-    </v-card>
-  </v-dialog>
+    <template #header>Delete caption?</template>
+    <template #body>
+      <div class="text-left">Are you sure you want to delete caption?</div>
+    </template>
+    <template #actions>
+      <v-btn @click="dialog = false" color="primary" text>Close</v-btn>
+      <v-btn @click="confirm" color="error" text>Delete caption</v-btn>
+    </template>
+  </tailor-dialog>
 </template>
 
 <script>
+import TailorDialog from '../tce-core/TailorDialog.vue';
+
 export default {
   name: 'confirmation-dialog',
   data: () => ({ dialog: false }),
@@ -31,6 +25,7 @@ export default {
       this.dialog = false;
       this.$emit('confirm');
     }
-  }
+  },
+  components: { TailorDialog }
 };
 </script>
